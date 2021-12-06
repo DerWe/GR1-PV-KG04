8948228dd618fa68cc2cc65d117a916254b1075b
DerWe <j.w-a@t-online.de>
2021-12-06 12:13:50 +0100

    Lösung angepasst. 2 Aufgabenteile bearbeitet

--- B.4.1.8 Typumwandlung/B.4.1.8 Typumwandlung.cpp
+++ B.4.1.8 Typumwandlung/B.4.1.8 Typumwandlung.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
 	int l;
 	double laenge{ 100.05 };
 	char zeichen;
+	// Es wird eine Zahl von double in int konvertiert, Double ist eine Kommazahl, die von Int nicht unterstützt wird. Nachkommastellen werden gerundet und weggelassen
 	l = laenge;
+	// Es wird eine Zahl von Double in Char konvertiert. Die Ersten 8 Bit werden übernommen.
 	zeichen = laenge;
 	cout << laenge << " " << l << " " << zeichen << endl;
 	string messlatte{ "777.77" };
