b02f85f2ef71547a64f5e6ffc19f62044778ec7d
DerWe <j.w-a@t-online.de>
2021-12-07 14:38:50 +0100

    Vollständige Beschreibung der Fehler in B.4.1.8 Eingefügt

--- B.4.1.8 Typumwandlung/B.4.1.8 Typumwandlung.cpp
+++ B.4.1.8 Typumwandlung/B.4.1.8 Typumwandlung.cpp
@@ -6,12 +6,13 @@ int main()
 	int l;
 	double laenge{ 100.05 };
 	char zeichen;
-	// Es wird eine Zahl von double in int konvertiert, Double ist eine Kommazahl, die von Int nicht unterstützt wird. Nachkommastellen werden gerundet und weggelassen
+	// Es wird eine Zahl von double in int konvertiert, Double ist eine Kommazahl, die von Int nicht unterstützt wird. Nachkommastellen werden gerundet und weggelassen.
 	l = laenge;
-	// Es wird eine Zahl von Double in Char konvertiert. Die Ersten 8 Bit werden übernommen.
+	// Es wird eine Zahl von Double in Char konvertiert. Die Ersten 8 Bit werden übernommen. Allerdings wird dadurch der Sinn verfälscht. Es handelt sich nicht mehr um die 1. Stelle der Zahl
 	zeichen = laenge;
 	cout << laenge << " " << l << " " << zeichen << endl;
 	string messlatte{ "777.77" };
+	// Die Zahl 777.77 wurde als string gespeichert und soll in double konvertiert werden. Allerdings gehört string nicht zu den elementaren Datentypen und wird daher von static_cast nicht unterstützt
 	laenge = static_cast<double>(messlatte);
 	return 1;
 }
