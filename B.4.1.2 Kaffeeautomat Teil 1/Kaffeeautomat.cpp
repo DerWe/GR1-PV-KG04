d59f59617492d0288fd0d1ae1bffd797ff5dade8
Aladin2310 <pascal-hammer@t-online.de>
2021-12-13 10:02:01 +0100

    Fallunterscheidung der drei Auswahlmöglichkeiten erstellt cin >> input gegen input = _getch() ausgetauscht damit keine weitere eingabe notwendig ist.

--- B.4.1.2 Kaffeeautomat Teil 1/Kaffeeautomat.cpp
+++ B.4.1.2 Kaffeeautomat Teil 1/Kaffeeautomat.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 //Hauptfunktion
 int main()
 {
+	//Variablendeklaration
 	char input;
 
+	//Clearen des User-Interfaces
 	system("cls");
+
+	//Textausgabe
 	cout << "Herzlich Willkommen beim Kaffee-Automaten!\n"
 			"Bitte w\x84hlen Sie aus:\n"
 			"(k) Kaffee\n"
 			"(e) Espresso\n"
 			"(s) Service-Mode\n";
-	cin >> input;
-	cout << "Sie haben sich f\x81r Auswahl " << input << " entschieden\n";
+	//cin >> input;
+	//Einlesen der ausgewählten option
+	input = _getch();
+
+	//Umwandlung der Auswahl.  Variable in einString und Ausgabe
+	switch (input)
+	{
+	case 'k':
+		cout << "Sie haben sich f\x81r Auswahl Kaffee entschieden.\n";
+		break;
+	case 'e':
+		cout << "Sie haben sich f\x81r Auswahl Espresso entschieden.\n";
+		break;
+	case 's':
+		cout << "Sie haben sich f\x81r Auswahl Service-Mode entschieden.\n";
+	default:
+		break;
+	}
 	system("pause");
 	return 0;
 }
