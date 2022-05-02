33d3ea0bb45a085b09f8f23872219147c9cd1db9
adrianwieg <adrian.wieg@sartorius.com>
2022-05-02 12:32:09 +0200

    Was bisher geschah: -CPP Datei in "main.cpp" umbenannt -Entwicklung der Klasse in Datum.h -Klasse Datum entworfen -Attribute erstelllt -Klassenmethode IstSchaltjahr() und TagDesJahres() erstellt und entwickelt

--- C.4.12.2 Klasse Datum/Datum.h
+++ C.4.12.2 Klasse Datum/Datum.h
@@ -1,1 +1,86 @@
 #pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class Datum
+{
+private:
+	unsigned int tag;
+	unsigned int monat;
+	unsigned int jahr;
+
+public:
+
+	//Standartkonstruktor
+	Datum(unsigned int Tag, unsigned int Monat, unsigned int Jahr)
+	{
+		tag = Tag;
+		monat = Monat;
+		jahr = Jahr;
+	}
+
+	//Klassenmethode IstSchaltjahr
+	static int IstSchaltjahr(int irgendeinJahr) {
+		if ((irgendeinJahr % 4) == 0) {
+			if ((irgendeinJahr % 100) != 0)	{
+				if ((irgendeinJahr % 400) == 0) {
+					return 1;
+				}
+				else {
+					return 0;
+				}
+			}
+			else {
+				return 0;
+			}
+		}
+		else {
+			return 0;
+		}
+	}
+
+	//Klassenmethode TagDesJahres
+	int TagDesJahres() {
+		int tagesanzahl = 0;
+
+		//Alle vollen Monate bis zum Datum Durchlaufen
+		for (int i_monat = 1; i_monat < this->monat; i_monat++) {
+
+			//Auswerten wie Viele Tage der gerade durchlaufene Monat hat
+			switch (this->monat)
+			{
+			//Monate mit 31 Tagen
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				tagesanzahl += 31;
+
+			//Monate mit 30 Tagen
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				tagesanzahl += 30;
+
+			//Separate betrachtung des Februar, aufgrund der Schaltjahresthematik
+			case 2:
+				if (IstSchaltjahr(this->jahr) == 1) {
+					tagesanzahl += 29;
+
+				}
+				else {
+					tagesanzahl += 28;
+				}
+			}
+		}
+		// Restliche Tage im angefangenen Monat hinzu addieren
+		tagesanzahl += this->tag;
+		return tagesanzahl;
+	}
+};
